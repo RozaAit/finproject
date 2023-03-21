74c3e89372e70a187f358fd99553de9887b48e3d
Roza Aitberova <rositka@yandex.ru>
2023-03-21 23:16:22 +0300

    написание программы

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿  string[] M;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            M = stroka.Split(' ');
+            var result = new string[M .Length];
+            string[] array1 = M;
+            string[] array2 = result;
+            void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
